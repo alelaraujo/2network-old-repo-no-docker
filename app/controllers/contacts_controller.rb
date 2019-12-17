3c33f351afb4d039d1b86437445b586f1f7f7b40
Alexandre Araujo <alelaraujo@gmail.com>
2019-12-17 20:34:29 +0000

    start components

---
+++ app/controllers/contacts_controller.rb
@@ -0,0 +1,20 @@
+class ContactsController < ApplicationController
+  before_action contact_params[only :new]
+  def create
+
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+  def contact_params
+    params.require(:contact).permit(:name, :address)
+  end
+end
