c0bb714a8dd0390ebdf94ba522c2af8ce35014af
Alexandre Araujo <alelaraujo@gmail.com>
2020-01-12 18:17:22 +0000

    fixed error on events controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,9 +9,12 @@
 # User.new(first_name: "Alexandre", last_name: "Araujo", email: "alexandre@araujo.me", password: "123456")
 User.destroy_all
 Contact.destroy_all
+Type.destroy_all
 
 User.create!(email: "alexandre@araujo.me", password: "123456")
 
 Contact.create!(user_id: User.first.id, first_name: "Simony", last_name: "Alcantara")
 Contact.create!(user_id: User.first.id, first_name: "André", last_name: "Araujo")
 Contact.create!(user_id: User.first.id, first_name: "Carina", last_name: "Carvalho")
+
+Type.create!(name: "Other", icon: "fas fa-question-circle")
