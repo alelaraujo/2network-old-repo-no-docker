df199db497ce75dd1f99bb8f6d6e59d44e826292
Alexandre Araujo <alelaraujo@gmail.com>
2020-02-27 13:26:39 +0000

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require 'faker'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -6,25 +7,38 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# User.new(first_name: "Alexandre", last_name: "Araujo", email: "alexandre@araujo.me", password: "123456")
+puts "Detroy all data..."
 User.destroy_all
+puts "Users destroyed"
 Contact.destroy_all
+puts "Contacts destroyed"
 Type.destroy_all
+puts "Types destroyed"
+Event.destroy_all
+puts "Events destroyed"
+puts ""
 
-User.create!(email: "alexandre@araujo.me", password: "123456")
-User.create!(email: "simony@araujo.me", password: "123456")
+type_id = []
+@type = Type.create!(name: "Message", icon: "fa-comments")
+type_id << @type.id
+@type = Type.create!(name: "Meeting", icon: "fa-calendar-day")
+type_id << @type.id
+@type = Type.create!(name: "Other", icon: "fa-question-circle")
+type_id << @type.id
+@type = Type.create!(name: "Phone", icon: "fa-phone-square")
+type_id << @type.id
 
-Contact.create!(user_id: User.first.id, first_name: "Simony", last_name: "Alcantara")
-Contact.create!(user_id: User.first.id, first_name: "André", last_name: "Araujo")
-Contact.create!(user_id: User.first.id, first_name: "Carina", last_name: "Carvalho")
+@user = User.create!(email: "test_a@gmail.com", password: "123456")
+puts "Created user #{@user.email}"
+rand(35..50).times do
+    @contact = Contact.create!(user_id: @user.id, first_name: Faker::Name.first_name, last_name: Faker::Name.last_name )
+    puts "Created contact #{@contact.id}"
+    rand(10..20).times do
+        @event = Event.create!(user_id: @user.id, contact_id: @contact.id,type_id: type_id[rand(0..3)], note: Faker::Lorem.paragraph_by_chars(number: rand(30..256), supplemental: false))
+        puts "Created event #{@event.id} to the contact #{@contact.id}"
+    end
+    puts ""
+end
 
-Contact.create!(user_id: User.last.id, first_name: "Irandir", last_name: "Alcantara")
-Contact.create!(user_id: User.last.id, first_name: "Estela", last_name: "Alcantara")
-Contact.create!(user_id: User.last.id, first_name: "Jake", last_name: "Dog")
-
-Type.create!(name: "Message", icon: "fa-comments")
-Type.create!(name: "Meeting", icon: "fa-calendar-day")
-Type.create!(name: "Other", icon: "fa-question-circle")
-Type.create!(name: "Phone", icon: "fa-phone-square")
 
 
