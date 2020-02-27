0a2d96dc11837329d70a818d838b3ab8ac6763e0
Alexandre Araujo <alelaraujo@gmail.com>
2020-02-27 11:50:12 +0000

    search contacts

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,4 +1,6 @@
 class Contact < ApplicationRecord
+  include PgSearch::Model
+  
   belongs_to :user
   has_many :events, dependent: :destroy
   has_one_attached :photo
@@ -6,4 +8,10 @@ class Contact < ApplicationRecord
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :first_name, uniqueness: { scope: :last_name }
+
+  pg_search_scope :search_by_first_name_and_last_name,
+    against: [ :first_name, :last_name ],
+    using: {
+      tsearch: { prefix: true } # <-- now `superman batm` will return something!
+    }
 end
